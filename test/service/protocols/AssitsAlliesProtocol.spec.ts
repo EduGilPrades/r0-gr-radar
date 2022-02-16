803ee9efc091e88638b35623f8f411bea4bba10c
Eduardo Gil <eduardo@comerciosweb.es>
2022-02-16 10:48:06 +0100

    feat: add assists allies protocol test

---
+++ test/service/protocols/AssitsAlliesProtocol.spec.ts
@@ -0,0 +1,25 @@
+import { expect } from "chai";
+import { AssistAlliesProtocol } from "../../../src/service/protocols/AssitsAlliesProtocol";
+import { FakeData } from "../helpers/FakeScans";
+
+describe("AssistsAlliesProtocol", () => {
+  let sut: AssistAlliesProtocol;
+
+  beforeEach(() => {
+    sut = new AssistAlliesProtocol();
+  });
+
+  it("should return only scans with allies if some have allies", () => {
+    let scans = FakeData.scans;
+    let scansWithAllies = FakeData.scansWithAllies;
+    const filteredScans = sut.apply(scans);
+    expect(filteredScans).to.be.deep.eq(scansWithAllies);
+  });
+
+  it("should return all scans if there aren't allies", () => {
+    let scans = FakeData.scansWithoutAllies;
+    let scansWithoutAllies = FakeData.scansWithoutAllies;
+    const filteredScans = sut.apply(scans);
+    expect(filteredScans).to.be.deep.eq(scansWithoutAllies);
+  });
+});
