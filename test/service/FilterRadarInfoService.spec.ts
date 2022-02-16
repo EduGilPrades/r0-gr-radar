389f6c31d15a9ffb8e541de8c3af653eea0fe037
Eduardo Gil <eduardo@comerciosweb.es>
2022-02-16 13:09:47 +0100

    feat: add filter radar info service test

---
+++ test/service/FilterRadarInfoService.spec.ts
@@ -0,0 +1,28 @@
+import { expect } from "chai";
+import { Protocol } from "../../src/core/domain/types/Protocol";
+import { Scan } from "../../src/core/domain/types/Scan";
+import { FilterRadarInfoService } from "../../src/service/FilterRadarInfoService";
+import { FakeData } from "./helpers/FakeScans";
+
+describe("FilterRadarInfoService", () => {
+  let scans: Scan[];
+  let sut: FilterRadarInfoService;
+
+  beforeEach(() => {
+    scans = FakeData.scans;
+  });
+
+  it("should return only one filtered scan when filter scans", () => {
+    let protocols: Protocol[] = [Protocol.ASSIST_ALLIES, Protocol.AVOID_MECH];
+    let filteredScan = {
+      enemies: { type: "soldier", number: 30 },
+      allies: 5,
+      coordinates: { y: 21, x: 38 },
+    };
+
+    sut = new FilterRadarInfoService(protocols, scans);
+
+    const bestTarget: Scan = sut.getBestTarget();
+    expect(bestTarget).to.be.deep.eq(filteredScan);
+  });
+});
