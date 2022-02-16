6cb188cdb543cf373984c997b6eb8fc14b61fc80
Eduardo Gil <eduardo@comerciosweb.es>
2022-02-16 10:48:39 +0100

    feat: add avoid crossfire protocol test

---
+++ test/service/protocols/AvoidCrossFireProtocol.spec.ts
@@ -0,0 +1,25 @@
+import { expect } from "chai";
+import { AvoidCrossFireProtocol } from "../../../src/service/protocols/AvoidCrossFireProtocol";
+import { FakeData } from "../helpers/FakeScans";
+
+describe("AvoidCrossFireProtocol", () => {
+  let sut: AvoidCrossFireProtocol;
+
+  beforeEach(() => {
+    sut = new AvoidCrossFireProtocol();
+  });
+
+  it("should return only scans without allies if some have allies", () => {
+    let scans = FakeData.scans;
+    let scansWithoutAllies = FakeData.scansWithoutAllies;
+    const filteredScans = sut.apply(scans);
+    expect(filteredScans).to.be.deep.eq(scansWithoutAllies);
+  });
+
+  it("should return all scans if all have allies", () => {
+    let scans = FakeData.scansWithAllies;
+    let scansWithAllies = FakeData.scansWithAllies;
+    const filteredScans = sut.apply(scans);
+    expect(filteredScans).to.be.deep.eq(scansWithAllies);
+  });
+});
