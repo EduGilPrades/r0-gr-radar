a34d9797577d4c496a94586676295206240050a2
Eduardo Gil <eduardo@comerciosweb.es>
2022-02-16 11:04:19 +0100

    feat: add closest enemies fake data test

--- test/service/helpers/FakeScans.ts
+++ test/service/helpers/FakeScans.ts
@@ -13,39 +13,39 @@ export class FakeData {
       coordinates: { y: 35, x: 11 },
     },
     {
-      enemies: { type: "soldier" as EnemiesType as EnemiesType, number: 10 },
+      enemies: { type: "soldier" as EnemiesType, number: 10 },
       coordinates: { y: 49, x: 19 },
     },
     {
-      enemies: { type: "soldier" as EnemiesType as EnemiesType, number: 30 },
+      enemies: { type: "soldier" as EnemiesType, number: 30 },
       allies: 5,
       coordinates: { y: 21, x: 38 },
     },
     {
-      enemies: { number: 30, type: "soldier" as EnemiesType as EnemiesType },
+      enemies: { number: 30, type: "soldier" as EnemiesType },
       allies: 8,
       coordinates: { x: 10, y: 39 },
     },
     {
-      enemies: { type: "soldier" as EnemiesType as EnemiesType, number: 15 },
+      enemies: { type: "soldier" as EnemiesType, number: 15 },
       coordinates: { x: 13, y: 38 },
     },
     {
-      enemies: { number: 60, type: "soldier" as EnemiesType as EnemiesType },
+      enemies: { number: 60, type: "soldier" as EnemiesType },
       coordinates: { x: 13, y: 15 },
     },
     {
-      enemies: { number: 40, type: "soldier" as EnemiesType as EnemiesType },
+      enemies: { number: 40, type: "soldier" as EnemiesType },
       coordinates: { y: 19, x: 30 },
     },
     {
       coordinates: { x: 30, y: 11 },
-      enemies: { number: 20, type: "soldier" as EnemiesType as EnemiesType },
+      enemies: { number: 20, type: "soldier" as EnemiesType },
     },
     {
       coordinates: { x: 15, y: 19 },
       allies: 11,
-      enemies: { number: 80, type: "soldier" as EnemiesType as EnemiesType },
+      enemies: { number: 80, type: "soldier" as EnemiesType },
     },
   ];
 
@@ -56,19 +56,19 @@ export class FakeData {
       coordinates: { y: 35, x: 11 },
     },
     {
-      enemies: { type: "soldier" as EnemiesType as EnemiesType, number: 30 },
+      enemies: { type: "soldier" as EnemiesType, number: 30 },
       allies: 5,
       coordinates: { y: 21, x: 38 },
     },
     {
-      enemies: { number: 30, type: "soldier" as EnemiesType as EnemiesType },
+      enemies: { number: 30, type: "soldier" as EnemiesType },
       allies: 8,
       coordinates: { x: 10, y: 39 },
     },
     {
       coordinates: { x: 15, y: 19 },
       allies: 11,
-      enemies: { number: 80, type: "soldier" as EnemiesType as EnemiesType },
+      enemies: { number: 80, type: "soldier" as EnemiesType },
     },
   ];
   public static scansWithoutAllies: Scan[] = [
@@ -77,24 +77,24 @@ export class FakeData {
       coordinates: { x: 89, y: 13 },
     },
     {
-      enemies: { type: "soldier" as EnemiesType as EnemiesType, number: 10 },
+      enemies: { type: "soldier" as EnemiesType, number: 10 },
       coordinates: { y: 49, x: 19 },
     },
     {
-      enemies: { type: "soldier" as EnemiesType as EnemiesType, number: 15 },
+      enemies: { type: "soldier" as EnemiesType, number: 15 },
       coordinates: { x: 13, y: 38 },
     },
     {
-      enemies: { number: 60, type: "soldier" as EnemiesType as EnemiesType },
+      enemies: { number: 60, type: "soldier" as EnemiesType },
       coordinates: { x: 13, y: 15 },
     },
     {
-      enemies: { number: 40, type: "soldier" as EnemiesType as EnemiesType },
+      enemies: { number: 40, type: "soldier" as EnemiesType },
       coordinates: { y: 19, x: 30 },
     },
     {
       coordinates: { x: 30, y: 11 },
-      enemies: { number: 20, type: "soldier" as EnemiesType as EnemiesType },
+      enemies: { number: 20, type: "soldier" as EnemiesType },
     },
   ];
   public static scansWithoutMech: Scan[] = [
@@ -143,6 +143,52 @@ export class FakeData {
       enemies: { type: "mech" as EnemiesType, number: 10 },
       allies: 3,
       coordinates: { y: 35, x: 11 },
+    },
+  ];
+  public static scansOrderedClosestFirst: Scan[] = [
+    {
+      enemies: { number: 60, type: "soldier" as EnemiesType },
+      coordinates: { x: 13, y: 15 },
+    },
+    {
+      coordinates: { x: 15, y: 19 },
+      allies: 11,
+      enemies: { number: 80, type: "soldier" as EnemiesType },
+    },
+    {
+      coordinates: { x: 30, y: 11 },
+      enemies: { number: 20, type: "soldier" as EnemiesType },
+    },
+    {
+      enemies: { number: 40, type: "soldier" as EnemiesType },
+      coordinates: { y: 19, x: 30 },
+    },
+    {
+      enemies: { type: "mech" as EnemiesType, number: 10 },
+      allies: 3,
+      coordinates: { y: 35, x: 11 },
+    },
+    {
+      enemies: { type: "soldier" as EnemiesType, number: 15 },
+      coordinates: { x: 13, y: 38 },
+    },
+    {
+      enemies: { number: 30, type: "soldier" as EnemiesType },
+      allies: 8,
+      coordinates: { x: 10, y: 39 },
+    },
+    {
+      enemies: { type: "soldier" as EnemiesType, number: 30 },
+      allies: 5,
+      coordinates: { y: 21, x: 38 },
+    },
+    {
+      enemies: { type: "soldier" as EnemiesType, number: 10 },
+      coordinates: { y: 49, x: 19 },
+    },
+    {
+      enemies: { number: 1, type: "mech" as EnemiesType },
+      coordinates: { x: 89, y: 13 },
     },
   ];
 }
